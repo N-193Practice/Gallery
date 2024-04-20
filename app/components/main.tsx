59ba15bd63a4ecdbfc4790fce7cdb1fd8c107783
Ikumi N <193nk365@gmail.com>
2024-04-20 16:27:11 +0900

    page.tsx(トップページ),layout.tsx(レイアウト),galleryパーツをcomponentsフォルダに作成

---
+++ app/components/main.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { StrictMode } from 'react'
+import { Canvas } from '@react-three/fiber'
+
+import * as THREE from 'three'
+import Basic from './basic'
+import Gallery from './gallery'
+
+// メイン...シーンを作成
+const Main = () => {
+  return (
+    <div className="w-full h-screen overflow-hidden">
+      <StrictMode>
+        <Canvas
+          flat
+          shadows
+          gl={{
+            antialias: true,
+            toneMapping: THREE.ACESFilmicToneMapping,
+            outputEncoding: THREE.sRGBEncoding,
+          }}
+          camera={{
+            fov: 45,
+            near: 0.1,
+            far: 100,
+            position: [0, 0, 4],
+          }}
+        >
+          {/* <Basic /> */}
+          <Gallery />
+        </Canvas>
+      </StrictMode>
+    </div>
+  )
+}
+
+export default Main
